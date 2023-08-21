75cad8303045f898608d4a83ffe7bc1078448271
colif74 <128209514+colif74@users.noreply.github.com>
2023-08-21 20:26:56 +0500

    Delete Pass directory

--- Pass/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import api
-
-urlpatterns = [
-    path('category', api.CategoryListAPIView.as_view(), name='api_categories'),
-    path('climber', api.ClimberListAPIView.as_view(), name='api_climbers'),
-    path('pereval', api.PerevalAddedListAPIView.as_view(), name='api_pereval'),
-]
-
